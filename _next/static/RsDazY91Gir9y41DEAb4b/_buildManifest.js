49b7afcea0acba0c59991916faa6aa76cf715c18
django-iori <django-iori@users.noreply.github.com>
2022-09-13 11:20:48 +0000

    deploy: ee6238d0f2827ec19a31880778ad865c12d0e682

--- _next/static/RsDazY91Gir9y41DEAb4b/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-a7b68aee0a64cecb.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
